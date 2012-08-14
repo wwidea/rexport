f9606a9bd98daed5e8d2596abb86d2a805669259
Jonathan Garvin <jgarvin@wwidea.org>
2012-08-14 17:15:30 -0600

    fix views

--- lib/rexport.rb
+++ lib/rexport.rb
@@ -7,3 +7,11 @@ require 'rexport/export_items_controller_methods'
 require 'rexport/export_methods'
 require 'rexport/exports_controller_methods'
 require 'rexport/tree_node'
+
+module Rexport
+
+  class Engine < ::Rails::Engine
+    config.paths.app.views << File.join(File.dirname(__FILE__),'../app/views')
+  end
+
+end
