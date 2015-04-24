40461d46ab85f5fa4645ba623adc0f1bfdef9772
Aaron Baldwin <baldwina@wwidea.org>
2015-04-24 09:18:43 -0600

    add form-control class to filter fields

--- app/helpers/exports_helper.rb
+++ app/helpers/exports_helper.rb
@@ -5,21 +5,24 @@ module ExportsHelper
     value = @export.filter_value(filter_field)
 
     case field.type
-      when :boolean
-        select_tag(tag_name, options_for_select([nil, ['true', 1], ['false', 0]], (value.to_i unless value.nil?)))
-      when :association
-        association, text_method = field.method.split('.')
-        select_tag( tag_name,
-              ('<option value=""></option>' +
-              options_from_collection_for_select(
-                rexport_model.collection_from_association(association),
-                :id,
-                text_method,
-                value.to_i)).html_safe)
-      when :datetime, nil
-        '&nbsp;'.html_safe
-      else
-        text_field_tag(tag_name, value)
+    when :boolean
+      select_tag(tag_name, options_for_select([nil, ['true', 1], ['false', 0]], (value.to_i unless value.nil?)), class: 'form-control')
+    when :association
+      association, text_method = field.method.split('.')
+      select_tag(tag_name,
+        ('<option value=""></option>' +
+        options_from_collection_for_select(
+          rexport_model.collection_from_association(association),
+          :id,
+          text_method,
+          value.to_i
+        )).html_safe,
+        class: 'form-control'
+      )
+    when :datetime, nil
+      '&nbsp;'.html_safe
+    else
+      text_field_tag(tag_name, value, class: 'form-control')
     end
   end
 end
