18a6807d1a5b281c6f560c315a2cd53739634bb5
Aaron Baldwin <baldwina@brightwayslearning.org>
2018-11-07 12:16:08 -0700

    refactor display_value

--- lib/rexport/export_filter_methods.rb
+++ lib/rexport/export_filter_methods.rb
@@ -8,15 +8,7 @@ module Rexport #:nodoc:
     end
 
     def display_value
-      return value unless filter_field[/_id$/]
-      association = find_association
-      return 'UNDEFINED ASSOCIATION' unless association
-      begin
-        object = association.klass.find(value)
-        return object.respond_to?(:name) ? object.name : object.to_s
-      rescue ActiveRecord::RecordNotFound
-        return 'ASSOCIATED OBJECT NOT FOUND'
-      end
+      filter_on_associated_object? ? associated_object_value : value
     end
 
     def attributes_for_copy
@@ -25,12 +17,40 @@ module Rexport #:nodoc:
 
     private
 
-    def find_association
-      path = filter_field.split('.')
-      foreign_key = path.pop
-      export.get_klass_from_path(path).reflect_on_all_associations(:belongs_to).detect do |association|
-        association.foreign_key == foreign_key
+    def associated_object_value
+      return 'UNDEFINED ASSOCIATION' unless filter_association
+      begin
+        object = filter_association.klass.find(value)
+        return object.respond_to?(:name) ? object.name : object.to_s
+      rescue ActiveRecord::RecordNotFound
+        return 'ASSOCIATED OBJECT NOT FOUND'
       end
+    end
+
+    def filter_association
+      @filter_on_assocation ||= find_filter_association
+    end
+
+    def find_filter_association
+      belongs_to_associations.detect do |association|
+        association.foreign_key == filter_foreign_key
+      end
+    end
+
+    def belongs_to_associations
+      export.get_klass_from_path(filter_path).reflect_on_all_associations(:belongs_to)
+    end
+
+    def filter_path
+      filter_field.split('.')[0..-2]
+    end
+
+    def filter_foreign_key
+      filter_field.split('.').last
+    end
+
+    def filter_on_associated_object?
+      filter_field[/_id$/]
     end
   end
 end
