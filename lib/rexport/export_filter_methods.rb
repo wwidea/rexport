7bac55eef9e8255470d3c92bac14c2fa39b308bd
Aaron Baldwin <baldwina@brightwayslearning.org>
2018-11-07 10:33:31 -0700

    update to use activesupport concern

--- lib/rexport/export_filter_methods.rb
+++ lib/rexport/export_filter_methods.rb
@@ -1,39 +1,35 @@
 module Rexport #:nodoc:
   module ExportFilterMethods
-    def self.included(base)
-      base.class_eval do
-        include InstanceMethods
+    extend ActiveSupport::Concern
 
-        belongs_to :export
-        validates_presence_of :filter_field
+    included do
+      belongs_to :export
+      validates_presence_of :filter_field
+    end
+
+    def display_value
+      return value unless filter_field[/_id$/]
+      association = find_association
+      return 'UNDEFINED ASSOCIATION' unless association
+      begin
+        object = association.klass.find(value)
+        return object.respond_to?(:name) ? object.name : object.to_s
+      rescue ActiveRecord::RecordNotFound
+        return 'ASSOCIATED OBJECT NOT FOUND'
       end
     end
 
-    module InstanceMethods
-      def display_value
-        return value unless filter_field[/_id$/]
-        association = find_association
-        return 'UNDEFINED ASSOCIATION' unless association
-        begin
-          object = association.klass.find(value)
-          return object.respond_to?(:name) ? object.name : object.to_s
-        rescue ActiveRecord::RecordNotFound
-          return 'ASSOCIATED OBJECT NOT FOUND'
-        end
-      end
+    def attributes_for_copy
+      attributes.slice('filter_field', 'value')
+    end
 
-      def attributes_for_copy
-        attributes.slice('filter_field', 'value')
-      end
+    private
 
-      private
-
-      def find_association
-        path = filter_field.split('.')
-        foreign_key = path.pop
-        export.get_klass_from_path(path).reflect_on_all_associations(:belongs_to).detect do |association|
-          association.foreign_key == foreign_key
-        end
+    def find_association
+      path = filter_field.split('.')
+      foreign_key = path.pop
+      export.get_klass_from_path(path).reflect_on_all_associations(:belongs_to).detect do |association|
+        association.foreign_key == foreign_key
       end
     end
   end
