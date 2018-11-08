9f49dfbc0eb06bc337e7fed55b9c45bebf72567d
Aaron Baldwin <baldwina@brightwayslearning.org>
2018-11-07 17:48:14 -0700

    use utc time to match factory object

--- test/factories.rb
+++ test/factories.rb
@@ -1,8 +1,8 @@
 module Rexport
   module Factories
-    
+
     FactoryGirl.define do
-    
+
       factory :status do
         name 'active'
       end
@@ -10,24 +10,24 @@ module Rexport
       factory :family do
         name 'The Sample Family'
       end
-      
+
       factory :student do
         family        {|f| f.association(:family)}
         name          'Sammy Sample'
         date_of_birth Date.parse('2008-12-08')
       end
-      
+
       factory :enrollment do
         status {|status| status.association(:status)}
         student {|s| s.association(:student)}
         grade   1
       end
-      
+
       factory :second_grade_enrollment, :class => 'Enrollment' do
         status {|status| status.association(:status)}
         grade 2
       end
-      
+
       factory :export do
         name 'Enrollment Export'
         model_class_name 'Enrollment'
@@ -37,7 +37,7 @@ module Rexport
           end
         end
       end
-      
+
       factory :filtered_export, :class => 'Export' do
         name 'Filtered Enrollment Export'
         model_class_name 'Enrollment'
@@ -47,36 +47,36 @@ module Rexport
           end
         end
       end
-      
+
       factory :family_name_export_item, :class => 'ExportItem' do
         position       1
         name           'Family Name'
         rexport_field  'student.family.name'
       end
-      
+
       factory :grade_export_item, :class => 'ExportItem' do
         position       2
         name           'Grade'
         rexport_field  'grade'
       end
-      
+
       factory :status_name_export_item, :class => 'ExportItem' do
         position       3
         name           'Status'
         rexport_field  'status_name'
       end
-      
+
       factory :bogus_export_item, :class => 'ExportItem' do
         position       4
         name           'Bogus Item'
         rexport_field  'bogus_field'
       end
-      
+
       factory :grade_filter, :class => 'ExportFilter' do
         filter_field   'grade'
         value          '1'
       end
-      
+
       factory :status_filter, :class => 'ExportFilter' do
         filter_field   'status.name'
         value          'active'
