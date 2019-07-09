809f57c090dac1d27507f89057be2f014cb8d348
Aaron Baldwin <baldwina@brightwayslearning.org>
2019-07-09 09:12:31 -0600

    extract object load into method

--- lib/rexport/export_filters_controller_methods.rb
+++ lib/rexport/export_filters_controller_methods.rb
@@ -1,10 +1,14 @@
 module Rexport
   module ExportFiltersControllerMethods
     def destroy
-      @export_filter = ExportFilter.find(params[:id])
-      @export_filter.destroy
-      
-      redirect_to @export_filter.export
+      export_filter.destroy
+      redirect_to export_filter.export
+    end
+
+    private
+
+    def export_filter
+      @export_filter ||= ExportFilter.find(params[:id])
     end
   end
 end
--- lib/rexport/export_items_controller_methods.rb
+++ lib/rexport/export_items_controller_methods.rb
@@ -1,28 +1,28 @@
 module Rexport
   module ExportItemsControllerMethods
     def edit
-      @export_item = ExportItem.find(params[:id])
+      export_item
     end
-    
+
     def update
-      @export_item = ExportItem.find(params[:id])
-      
-      if @export_item.update_attributes(export_item_params)
-        redirect_to export_path(@export_item.export), notice: 'ExportItem was successfully updated.'
+      if export_item.update_attributes(export_item_params)
+        redirect_to export_path(export_item.export), notice: 'ExportItem was successfully updated.'
       else
         render :edit
       end
     end
-    
+
     def destroy
-      @export_item = ExportItem.find(params[:id])
-      @export_item.destroy
-      
-      redirect_to export_path(@export_item.export)
+      export_item.destroy
+      redirect_to export_path(export_item.export)
     end
-    
+
     private
-    
+
+    def export_item
+      @export_item ||= ExportItem.find(params[:id])
+    end
+
     def export_item_params
       params.require(:export_item).permit(:name)
     end
