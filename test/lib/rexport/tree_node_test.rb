73e9c9f37299fe964c2289587ff5694b0afeead1
Aaron Baldwin <baldwina@brightwayslearning.org>
2023-02-21 10:21:24 -0700

    rubocop updates

--- test/lib/rexport/tree_node_test.rb
+++ test/lib/rexport/tree_node_test.rb
@@ -5,12 +5,14 @@ require "test_helper"
 class TreeNodeTest < ActiveSupport::TestCase
   test "should initialize with name" do
     t = Rexport::TreeNode.new("test")
+
     assert_equal "test", t.name
-    assert_equal [], t.children
+    assert_empty t.children
   end
 
   test "should initialize with name and children" do
     t = Rexport::TreeNode.new("test", %w[one two three])
+
     assert_equal "test", t.name
     assert_equal ["test", [["one", [["two", [["three", []]]]]]]], t.to_a
   end
@@ -18,28 +20,38 @@ class TreeNodeTest < ActiveSupport::TestCase
   test "should add children" do
     root = Rexport::TreeNode.new("root")
     root.add_child("a")
+
     assert_equal ["root", [["a", []]]], root.to_a
+
     root.add_child("a")
+
     assert_equal ["root", [["a", []]]], root.to_a
-    root.add_child("a", [1 , 2])
+
+    root.add_child("a", [1, 2])
+
     assert_equal ["root", [["a", [[1, [[2, []]]]]]]], root.to_a
   end
 
   test "should return empty include" do
     root = Rexport::TreeNode.new("root")
-    assert_equal [], root.to_include
+
+    assert_empty root.to_include
   end
 
   test "should return single level array include" do
     root = Rexport::TreeNode.new("root")
     %w[a b c].each { |l| root.add_child(l) }
-    assert_equal [:a, :b, :c], root.to_include
+
+    assert_equal %i[a b c], root.to_include
   end
 
   test "should return nested hash include" do
     root = Rexport::TreeNode.new("root", %w[a b c])
+
     assert_equal [{ a: [{ b: [:c] }] }], root.to_include
+
     root.add_child "b", "one", "two"
-    assert_equal [{ a: [{ b: [:c] }] }, { b: [{ one:[:two] }] }], root.to_include
+
+    assert_equal [{ a: [{ b: [:c] }] }, { b: [{ one: [:two] }] }], root.to_include
   end
 end
