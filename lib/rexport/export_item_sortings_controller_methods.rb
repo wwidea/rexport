374eac0e3691e593e9d2f2918b35f71cb959a28d
Aaron Baldwin <baldwina@brightwayslearning.org>
2023-03-02 06:53:57 -0700

    remove spaces

--- lib/rexport/export_item_sortings_controller_methods.rb
+++ lib/rexport/export_item_sortings_controller_methods.rb
@@ -4,7 +4,7 @@ module Rexport
   module ExportItemSortingsControllerMethods
     def update
       ExportItem.resort(params[:sorted_items])
-    
+
       respond_to do |format|
         format.js do
           head :ok
