de92686f4488df325233bbf632d60476d9b16dde
Aaron Baldwin <baldwina@brightwayslearning.org>
2023-03-06 06:37:20 -0700

    add association_name method

--- test/lib/rexport/data_field_test.rb
+++ test/lib/rexport/data_field_test.rb
@@ -4,25 +4,36 @@ require "test_helper"
 
 class DataFieldTest < ActiveSupport::TestCase
   test "should stringify name" do
-    assert_equal "foo", Rexport::DataField.new(:foo).name
+    assert_equal "foo", data_field.name
   end
 
   test "should use name for method" do
-    assert_equal "foo", Rexport::DataField.new(:foo).method
+    assert_equal "foo", data_field.method
   end
 
   test "should save method" do
-    assert_equal "bar", Rexport::DataField.new(:foo, method: :bar).method
+    assert_equal "bar", data_field(method: :bar).method
   end
 
   test "should save type" do
-    assert_equal :type_test, Rexport::DataField.new(:test, type: :type_test).type
+    assert_equal :type_test, data_field(type: :type_test).type
   end
 
   test "should_sort_data_fields" do
-    a = Rexport::DataField.new(:a)
-    b = Rexport::DataField.new(:b)
+    assert_equal %w[a b], [data_field(name: :b), data_field(name: :a)].sort.map(&:name)
+  end
 
-    assert_equal [a, b], [b, a].sort
+  test "should return first assocation from method chain for association_name" do
+    assert_equal "assocation_one", data_field(method: "assocation_one.assocation_two.method").association_name
+  end
+
+  test "should return nil for association_name when no dot operator is present" do
+    assert_nil data_field(method: "method").association_name
+  end
+
+  private
+
+  def data_field(name: :foo, method: nil, type: nil)
+    Rexport::DataField.new(name, method: method, type: type)
   end
 end
